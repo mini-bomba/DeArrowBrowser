903fee5bf034ca3a71114fd13c863ed4898e51cc
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2023-07-21 23:14:52 +0200

    Add server configs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{path::Path, sync::RwLock, ops::DerefMut};
+use std::{sync::RwLock, ops::DerefMut, fs::File, io::{Read, Write, self}};
 use actix_files::Files;
 use actix_web::{HttpServer, App, web};
 use anyhow::{Context, anyhow};
@@ -11,11 +11,27 @@ mod state;
 mod api_models;
 use state::*;
 
+const CONFIG_PATH: &str = "config.toml";
+
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
-    let config = web::Data::new(AppConfig {
-        mirror_path: Path::new("/tmp").into(),
+    let config: web::Data<AppConfig> = web::Data::new(match File::open(CONFIG_PATH) {
+        Ok(mut file) => {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents).with_context(|| format!("Failed to read {CONFIG_PATH}"))?;
+            toml::from_str(&contents).with_context(|| format!("Failed to deserialize contents of {CONFIG_PATH}"))?
+        },
+        Err(e) if e.kind() == io::ErrorKind::NotFound => {
+            let cfg = AppConfig::default();
+            let serialized = toml::to_string(&cfg).context("Failed to serialize default AppConfig as TOML")?;
+            let mut file = File::options().write(true).create_new(true).open(CONFIG_PATH).with_context(|| format!("Failed to create {CONFIG_PATH}"))?;
+            write!(file, "{serialized}").with_context(|| format!("Failed to write serialized default AppConfig to {CONFIG_PATH}"))?;
+            cfg
+        },
+        Err(e) => {
+            return Err(e).context(format!("Failed to open {CONFIG_PATH}"));
+        }
     });
     let string_set = web::Data::new(RwLock::new(StringSet::with_capacity(16384)));
     let (db, errors) = DearrowDB::load_dir(&config.mirror_path, string_set.write().map_err(|_| anyhow!("Failed to aquire StringSet lock for writing"))?.deref_mut()).context("Initial DearrowDB load failed")?;
@@ -28,17 +44,18 @@ async fn main() -> anyhow::Result<()> {
         updating_now: false
     }));
 
+    let config_copy = config.clone();
     HttpServer::new(move || {
         App::new()
             .service(web::scope("/api")
                 .configure(routes::configure_routes)
-                .app_data(config.clone())
+                .app_data(config_copy.clone())
                 .app_data(db.clone())
                 .app_data(string_set.clone())
             )
             .service(Files::new("/", "static").index_file("index.html"))
     })
-    .bind(("127.0.0.1", 9292)).context("Failed to bind to 127.0.0.1:9292")?
+    .bind((config.listen.ip.as_str(), config.listen.port)).with_context(|| format!("Failed to bind to {}:{}", config.listen.ip.as_str(), config.listen.port))?
     .run()
     .await
     .context("Error while running the server")
--- src/state.rs
+++ src/state.rs
@@ -1,9 +1,42 @@
+use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine};
 use dearrow_browser::DearrowDB;
 use anyhow::Error;
-use std::path::Path;
+use getrandom::getrandom;
+use std::path::PathBuf;
+use serde::{Serialize, Deserialize};
 
+#[derive(Serialize, Deserialize)]
 pub struct AppConfig {
-    pub mirror_path: Box<Path>,
+    pub mirror_path: PathBuf,
+    pub listen: ListenConfig,
+    pub auth_secret: String,
+}
+
+impl Default for AppConfig {
+    fn default() -> Self {
+        let mut buffer: Vec<u8> = (0..32).map(|_| 0u8).collect();
+        getrandom(&mut buffer[..]).unwrap();
+        Self {
+            mirror_path: PathBuf::from("./mirror"),
+            listen: ListenConfig::default(),
+            auth_secret: URL_SAFE_NO_PAD.encode(buffer),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ListenConfig {
+    pub ip: String,
+    pub port: u16,
+}
+
+impl Default for ListenConfig {
+    fn default() -> Self {
+        Self {
+            ip: "0.0.0.0".to_owned(),
+            port: 9292,
+        }
+    }
 }
 
 pub struct DatabaseState {

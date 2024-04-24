932a1a5832c11ecb0b4c22e5ad4efa69316b8cba
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2024-04-24 21:57:00 +0200

    Add a loading page
    This should calm down the users who have slow internet connections
    
    also this commit introduces the first lines of javascript code into this
    repository
    also the progressbar is a bit useless due to a trunk bug

---
+++ dearrow-browser-frontend/initializer.mjs
@@ -0,0 +1,35 @@
+export default function() {
+  return {
+    onStart: () => {
+      const detail = document.getElementById("load-detail");
+      detail.innerText = "we're starting the WASM download";
+    },
+    onProgress: ({current, total}) => {
+      const progress = document.getElementById("load-progress");
+      const detail = document.getElementById("load-detail");
+        const current_kB = Math.floor(current / 100) / 10;
+
+      if (total != 0) {
+        const total_kB = Math.floor(total / 100) / 10;
+        progress.max = total;
+        progress.value = current;
+        detail.innerText = `${current_kB} kB / ${total_kB} kB`;
+      } else {
+        detail.innerText = `${current_kB} kB / nobody knows`;
+      }
+    },
+    onComplete: () => {},
+    onSuccess: () => {
+      document.getElementById("loading")?.remove();
+    },
+    onFailure: e => {
+      document.getElementById("load-detail").remove();
+      document.getElementById("load-progress").remove();
+      document.getElementById("load-header").innerText = "DeArrow Browser failed to initialize";
+      document.getElementById("load-text").innerText = "Make sure your browser supports WASM and try hard-refreshing (usually by clicking reload while holding shift)\n\nDetails about the error should appear below.";
+      const err_detail = document.createElement("pre");
+      err_detail.innerText = `${e.toString()}\n\nStack trace:\n${e.stack}`;
+      document.getElementById("loading").appendChild(err_detail);
+    }
+  }
+}

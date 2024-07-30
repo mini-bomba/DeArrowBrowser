e967b7627269e71a54fe6b0fc97c2427482dc117
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2024-07-31 01:30:37 +0200

    frontend: settings: undo&reset buttons
    macro abuse!!

--- dearrow-browser-frontend/src/components/modals/settings.rs
+++ dearrow-browser-frontend/src/components/modals/settings.rs
@@ -27,7 +27,7 @@ use crate::contexts::SettingsContext;
 /// Generator macro for a revert callback (Esc key pressed)
 ///
 /// Takes in the name of the settings field and a function to verify the input field's value
-macro_rules! revert_callback {
+macro_rules! esc_callback {
     ($name:ident, $verify_func:ident) => {
         move |e: KeyboardEvent, settings_context| {
             if e.key() == "Escape" {
@@ -40,6 +40,62 @@ macro_rules! revert_callback {
                 }
             }
         }
+    };
+}
+
+/// Generator macro for the undo button callback
+///
+/// Takes in the name of the settings field and a function to verify the input field's value
+macro_rules! undo_callback {
+    ($name:ident, $verify_func:ident) => {
+        move |_: MouseEvent, (settings_context, initial_settings, node_ref)| {
+            let mut settings = settings_context.settings().clone();
+            let target: HtmlInputElement = node_ref.cast().expect("Expected the NodeRef to be an HtmlInputElement");
+            target.set_value(&initial_settings.$name.to_string());
+            if $verify_func(&target).is_none() {
+                target.set_value(&settings_context.default.$name.to_string());
+                assert!($verify_func(&target).is_some(), stringify!(Default value of $name setting was invalid!));
+                settings.$name = settings_context.default.$name.clone();
+            } else {
+                settings.$name = initial_settings.$name.clone();
+            }
+            settings_context.update(settings);
+        }
+    };
+}
+
+/// Generator macro for a check to determine whether the undo button should be visible
+///
+/// Takes in the name of the settings field, the current and the initial settings
+macro_rules! should_show_undo {
+    ($name: ident, $current_settings:expr, $initial_settings:expr) => {
+        $current_settings.$name != $initial_settings.$name
+    };
+}
+
+/// Generator macro for the reset to default button callback
+///
+/// Takes in the name of the settings field and a function to verify the input field's value
+macro_rules! reset_callback {
+    ($name:ident, $verify_func:ident) => {
+        move |_: MouseEvent, (settings_context, node_ref)| {
+            let mut settings = settings_context.settings().clone();
+            let target: HtmlInputElement = node_ref.cast().expect("Expected the NodeRef to be an HtmlInputElement");
+            target.set_value(&settings_context.default.$name.to_string());
+            assert!($verify_func(&target).is_some(), stringify!(Default value of $name setting was invalid!));
+            settings.$name = settings_context.default.$name.clone();
+            settings_context.update(settings);
+        }
+    };
+}
+
+/// Generator macro for a check to determine whether the reset button should be visible
+///
+/// Takes in the name of the settings field, the current and the settings context (for default
+/// settings)
+macro_rules! should_show_reset {
+    ($name: ident, $current_settings:expr, $settings_context:expr) => {
+        $current_settings.$name != $settings_context.default.$name
     };
 }
 
@@ -146,16 +202,39 @@ pub fn SettingsModal() -> Html {
     let initial_settings = use_memo((), |()| settings_context.settings().clone());
     let current_settings = settings_context.settings();
 
+    let entries_per_page_ref = use_node_ref();
+    let thumbgen_api_base_url_ref = use_node_ref();
+
     let nonzerousize_oninput = use_callback((), move |e: InputEvent, ()| {
         nonzerousize_verify(&e.target_unchecked_into());
     });
     let baseurl_oninput = use_callback((), move |e: InputEvent, ()| {
         baseurl_verify(&e.target_unchecked_into());
     });
-    let entries_per_page_revert = use_callback(settings_context.clone(), revert_callback!(entries_per_page, nonzerousize_verify));
-    let thumbgen_api_base_url_revert = use_callback(settings_context.clone(), revert_callback!(thumbgen_api_base_url, baseurl_verify));
+
+    let entries_per_page_revert = use_callback(settings_context.clone(), esc_callback!(entries_per_page, nonzerousize_verify));
+    let thumbgen_api_base_url_revert = use_callback(settings_context.clone(), esc_callback!(thumbgen_api_base_url, baseurl_verify));
+
     let entries_per_page_save = use_callback(settings_context.clone(), save_callback!(entries_per_page, nonzerousize_verify));
     let thumbgen_api_base_url_save = use_callback(settings_context.clone(), save_callback!(thumbgen_api_base_url, baseurl_verify));
+
+    let entries_per_page_undo = use_callback(
+        (settings_context.clone(), initial_settings.clone(), entries_per_page_ref.clone()), 
+        undo_callback!(entries_per_page, nonzerousize_verify)
+    );
+    let thumbgen_api_base_url_undo = use_callback(
+        (settings_context.clone(), initial_settings.clone(), thumbgen_api_base_url_ref.clone()), 
+        undo_callback!(thumbgen_api_base_url, baseurl_verify)
+    );
+
+    let entries_per_page_reset = use_callback(
+        (settings_context.clone(), entries_per_page_ref.clone()), 
+        reset_callback!(entries_per_page, nonzerousize_verify)
+    );
+    let thumbgen_api_base_url_reset = use_callback(
+        (settings_context.clone(), thumbgen_api_base_url_ref.clone()), 
+        reset_callback!(thumbgen_api_base_url, baseurl_verify)
+    );
 
     html! {
         <div id="settings-modal">
@@ -170,8 +249,23 @@ pub fn SettingsModal() -> Html {
                     oninput={nonzerousize_oninput} 
                     onkeydown={entries_per_page_revert} 
                     onchange={entries_per_page_save} 
+                    ref={entries_per_page_ref}
                     ~value={current_settings.entries_per_page.to_string()} 
                 />
+                <div class="setting-actions">
+                    if should_show_undo!(entries_per_page, current_settings, initial_settings) {
+                        <span 
+                            class="clickable" title="Undo"
+                            onclick={entries_per_page_undo}
+                        >{"↩️"}</span>
+                    }
+                    if should_show_reset!(entries_per_page, current_settings, settings_context) {
+                        <span 
+                            class="clickable" title="Reset to default"
+                            onclick={entries_per_page_reset}
+                        >{"🔄"}</span>
+                    }
+                </div>
             </fieldset>
             <fieldset>
                 <legend>{"Thumbnail generator"}</legend>
@@ -183,8 +277,23 @@ pub fn SettingsModal() -> Html {
                     oninput={baseurl_oninput} 
                     onkeydown={thumbgen_api_base_url_revert} 
                     onchange={thumbgen_api_base_url_save} 
+                    ref={thumbgen_api_base_url_ref}
                     ~value={current_settings.thumbgen_api_base_url.to_string()} 
                 />
+                <div class="setting-actions">
+                    if should_show_undo!(thumbgen_api_base_url, current_settings, initial_settings) {
+                        <span 
+                            class="clickable" title="Undo"
+                            onclick={thumbgen_api_base_url_undo}
+                        >{"↩️"}</span>
+                    }
+                    if should_show_reset!(thumbgen_api_base_url, current_settings, settings_context) {
+                        <span 
+                            class="clickable" title="Reset to default"
+                            onclick={thumbgen_api_base_url_reset}
+                        >{"🔄"}</span>
+                    }
+                </div>
             </fieldset>
         </div>
     }
